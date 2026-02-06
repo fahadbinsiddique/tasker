8b12f85fd5dcf243bc1041152a5ad8891b7cc592
fahadbinsiddique <fahad.bin.siddique345@gmail.com>
2026-02-06 17:06:57 +0600

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./Header";
+
 const App = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-blue-600">Hello World</h1>
+      <Header/>
     </div>
   );
 };

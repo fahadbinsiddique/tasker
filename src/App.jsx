08988312ba92669a8c706077aa07f5aa3f9a4a9b
fahadbinsiddique <fahad.bin.siddique345@gmail.com>
2026-02-08 11:43:49 +0600

    task added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import Hero from "./Hero";
+import TaskBoard from "./task/TaskBoard";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Hero />
+      <div className="flex flex-col justify-center items-center">
+        <Hero />
+        <TaskBoard />
+      </div>
       <Footer />
     </div>
   );
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,14 @@
-import LWSLogo from './assets/image1.png';
+import LWSLogo from "./assets/react.svg";
 
 const Header = () => {
   return (
     <>
       <nav className="py-6 md:py-8 fixed top-0 w-full bg-[#191D26]! z-50">
-        <div className="container mx-auto flex items-center justify-between gap-x-6">
+        <div className="container mx-auto flex items-center  gap-x-3">
           <a href="/">
             <img className="h-11.25 " src={LWSLogo} alt="Lws" />
           </a>
+          <h1 className="font-semibold text-[23px]">Fahad</h1>
         </div>
       </nav>
     </>

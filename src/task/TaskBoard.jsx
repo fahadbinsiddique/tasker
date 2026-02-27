7baca222f880e707fc864f443161af5877f49b5a
fahadbinsiddique <fahad.bin.siddique345@gmail.com>
2026-02-28 02:12:48 +0600

    addModal

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
+import AddTaskModal from "./addTaskModal";
 
 const TaskBoard = () => {
   const defaultTask = {
@@ -15,16 +16,25 @@ const TaskBoard = () => {
   };
   const [tasks, setTasks] = useState([defaultTask]);
 
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  function handleAddTask(newTask) {
+    setTasks([...tasks, newTask]);
+
+    setShowAddModal(false);
+  }
+
   return (
     <>
       <section className="mb-20" id="tasks">
+        {showAddModal && <AddTaskModal onSave={handleAddTask} />}
         <div className="container">
           <div className="p-2 flex justify-end">
             <SearchTask />
           </div>
 
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            <TaskAction />
+            <TaskAction onAddClick={() => setShowAddModal(true)} />
             <div className="overflow-auto">
               <TaskList tasks={tasks} />
             </div>
